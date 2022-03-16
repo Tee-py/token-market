7fb857d87573ada1a38a762038a5e2f5c964db3c
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-03-16 16:55:35 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,11 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [owner, addr1] = await ethers.getSigners();
   const PriceFeed = await ethers.getContractFactory("PriceFeed");
   const USDT = await ethers.getContractFactory("USDT");
   const WETH = await ethers.getContractFactory("WETH");
-
+  const TokenSwap = await ethers.getContractFactory("TokenSwap");
 
   const priceFeed = await PriceFeed.deploy();
   await priceFeed.deployed();
@@ -13,10 +14,13 @@ async function main() {
   const usdt = await USDT.deploy();
   await usdt.deployed();
   const price = await priceFeed.getLatestPriceAndDecimal()
+  const tokenSwap = await TokenSwap.deploy(usdt.address, weth.address, owner.address, priceFeed.address);
+  await tokenSwap.deployed();
 
   console.log("PriceFeed deployed to:", priceFeed.address);
-  console.log(`WETH TOKEN DEPLOYED AT ${weth.address}`)
-  console.log(`USDT TOKEN DEPLOYED AT ${usdt.address}`)
+  console.log(`WETH TOKEN DEPLOYED AT ${weth.address}`);
+  console.log(`USDT TOKEN DEPLOYED AT ${usdt.address}`);
+  console.log(`TOKENSWAP DEPLOYED AT ${tokenSwap.address}`);
   console.log(`The price of 1USDT/ETH is ${price}`);
 }
 
