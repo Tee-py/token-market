2783f0332cbb1cffccec5b094a84cbb0aeb205b8
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-03-26 19:40:14 +0100

    done

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -22,11 +22,15 @@ contract TokenSwap {
         reserve = payable(_reverve);
     }
 
-    function swapToken(uint amount) payable {
+    function swapToken(uint amount) {
         (price, decimals) = feed.getLatestPriceAndDecimal();
-        require(msg.value >= amount*price*decimals, "Insufficient Value for Swap");
-        usdt.transferFrom(msg.sender, reserve, amount*10**18);
-        weth.transferFrom(reserve, msg.sender, amount*10**18);
+        uint usdtToPay = price*amount;
+        require(usdt.allowance(msg.sender, address(this)>=usdtToPay), "Insufficient Allowance for Swap");
+        require(weth.balanceOf(reserve)>=amount*10**18, "Insufficient Balance In Reserve");
+        bool res = usdt.transferFrom(msg.sender, reserve, usdtToPay);
+        require(res, "Failed To Send USDT");
+        bool res2 = weth.transferFrom(reserve, msg.sender, amount*10**18);
+        require(res2, "Failed To Send WETH");
         emit Swap(msg.sender, amount);
     }
 
